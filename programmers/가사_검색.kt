5013577743510b38b9bd31880aa3c5c75f3634a0
CmplxN <zayne@naver.com>
2020-04-30 13:18:35 +0900

    Create 가사_검색.kt

---
+++ programmers/가사_검색.kt
@@ -0,0 +1,33 @@
+data class Trie(val next:MutableMap<Char,Trie>, var cnt: Int = 0, var fin:Boolean = false) {
+    fun insert(str: String, idx: Int, end: Int) {
+        cnt++
+        if (idx == end) {
+            fin = true
+            return
+        }
+        next.computeIfAbsent(str[idx]) { Trie(mutableMapOf()) }
+        next[str[idx]]!!.insert(str, idx + 1, end)
+    }
+
+    fun search(str: String, idx: Int, end: Int): Int {
+        if (idx == end) return cnt
+        if (str[idx] != '?') return next[str[idx]]?.search(str, idx + 1, end) ?: 0
+        return next.map { it.value.cnt }.sum()
+    }
+}
+class Solution {
+    fun solution(words: Array<String>, queries: Array<String>): IntArray {
+        val nTrie = Array(10001) { Trie(mutableMapOf()) }
+        val rTrie = Array(10001) { Trie(mutableMapOf()) }
+        words.forEach {
+            nTrie[it.length].insert(it, 0, it.length)
+            rTrie[it.length].insert(it.reversed(), 0, it.length)
+        }
+        return queries.map {
+            when {
+                it.last() == '?' -> nTrie[it.length].search(it, 0, it.length)
+                else -> rTrie[it.length].search(it.reversed(), 0, it.length)
+            }
+        }.toIntArray()
+    }
+}
