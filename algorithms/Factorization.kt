1cd9e3a89196b25a7cc557924edfddce719629ee
CmplxN <zayne@naver.com>
2020-05-13 16:41:49 +0900

    Create Factorization.kt

---
+++ algorithms/Factorization.kt
@@ -0,0 +1,89 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.math.BigInteger
+import kotlin.random.Random
+import kotlin.random.nextUInt
+
+val random = Random(6523)
+
+fun function(x: BigInteger):BigInteger = x * x
+
+tailrec fun gcd(x:BigInteger,y:BigInteger):BigInteger {
+    if (y == BigInteger.ZERO) return x
+    return gcd(y, x.mod(y))
+}
+
+val tester = arrayOf(BigInteger("17"),BigInteger("13"),BigInteger("11"),
+    BigInteger("5"),BigInteger("3"),BigInteger("2"),BigInteger("7"),BigInteger("19"),
+    BigInteger("23"),BigInteger("29"),BigInteger("31"),BigInteger("37"),BigInteger("41"))
+
+fun millerRabin(n:BigInteger, a:BigInteger):Boolean {
+    var temp = n - BigInteger.ONE
+    while (temp % BigInteger("2") == BigInteger.ZERO) {
+        if (a.modPow(temp, n) == n - BigInteger.ONE)
+            return true
+        temp /= BigInteger("2")
+    }
+    val rst = a.modPow(temp, n)
+    return rst == n - BigInteger.ONE || rst == BigInteger.ONE
+}
+
+fun primeTester(num : BigInteger):Boolean {
+    if (num <= BigInteger.ONE) return false
+    if (num.mod(BigInteger("2")) == BigInteger.ZERO) return false
+    if (num <= BigInteger("10000")) {
+        val i = num.toInt()
+        var j = 2
+        while (j * j <= i) {
+            if (i % j == 0)
+                return false
+            j++
+        }
+        return true
+    }
+    for (test in tester)
+        if (!millerRabin(num, test))
+            return false
+    return true
+}
+
+fun PollardRho(n: BigInteger):BigInteger {
+    if(primeTester((n))) return n
+    if(n== BigInteger.ONE) return BigInteger.ONE
+    if(n.mod(BigInteger("2"))== BigInteger.ZERO) return BigInteger("2")
+    var x = BigInteger((random.nextUInt()).toString()).mod(n - BigInteger("2")) + BigInteger("2")
+    var y = x
+    var p = BigInteger.ONE
+    val k = BigInteger((random.nextUInt()).toString()).mod(n - BigInteger("1")) + BigInteger("1")
+    while (p == BigInteger.ONE) {
+        x = (function(x) + k).mod(n)
+        y = (function(y) + k).mod(n)
+        y = (function(y) + k).mod(n)
+        p = gcd((x - y).abs(), n)
+        if (p == n) return PollardRho(n)
+    }
+    if(!primeTester(p)) return PollardRho(p)
+    return p
+}
+
+fun main()=BufferedReader(InputStreamReader(System.`in`)).run {
+
+    var outer = BigInteger(readLine()!!)
+    if(outer == BigInteger.ONE){
+        print(1)
+        return
+    }
+    var inner: BigInteger
+    val list = mutableListOf<BigInteger>()
+    while(outer != BigInteger.ONE) {
+        inner = outer
+        while (inner > BigInteger.ONE && !primeTester(inner)) {
+            val result = PollardRho(inner)
+            inner /= result
+        }
+        if(inner > BigInteger.ONE) list.add(inner)
+        outer /= inner
+    }
+    list.sort()
+    println(list.joinToString("\n"))
+}
