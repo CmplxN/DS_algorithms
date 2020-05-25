da531bc26ed0cdb9d6fe28cb290935a210dd13bb
CmplxN <zayne@naver.com>
2020-05-25 10:33:42 +0900

    Create BOJ_1365.kt

---
+++ BOJ/BOJ_1365.kt
@@ -0,0 +1,36 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+
+typealias Index = Int
+
+val lis = mutableListOf<Int>()
+
+fun <T:Comparable<T>> lowerBound(elements: List<T>, l: Index, r: Index, value: T) : Index {
+    var left = l
+    var right = r
+    var mid: Index
+    while (left < right) {
+        mid = (left + right) shr 1
+        if (mid == r)
+            return r
+        if (elements[mid] < value)
+            left = mid + 1
+        else
+            right = mid
+    }
+    return left
+}
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine()!!.toInt()
+    val st = StringTokenizer(readLine()!!)
+    repeat(n) {
+        val input = st.nextToken()!!.toInt()
+        if (lis.size == 0 || input > lis.last())
+            lis.add(input)
+        val idx = lowerBound(lis, 0, lis.size, input)
+        lis[idx] = input
+    }
+    print(n - lis.size)
+}
