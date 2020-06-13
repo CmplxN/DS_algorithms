4361fc7f19dd552d4bdf3e0ed8c0d20a0cc2d185
CmplxN <zayne@naver.com>
2020-06-13 22:17:27 +0900

    Create DeepCopy.kt

---
+++ dataStructure/DeepCopy.kt
@@ -0,0 +1,11 @@
+fun <T : Serializable> deepCopy(obj: T?): T? {
+    if (obj == null) return null
+    val baos = ByteArrayOutputStream()
+    val oos  = ObjectOutputStream(baos)
+    oos.writeObject(obj)
+    oos.close()
+    val bais = ByteArrayInputStream(baos.toByteArray())
+    val ois  = ObjectInputStream(bais)
+    @Suppress("unchecked_cast")
+    return ois.readObject() as T
+}
